1ca59b8ab854b27342700c102674ed756d82569f
smitha5ten <smitha.madhav@gds.ey.com>
2020-02-17 11:51:53 +0000

    day 1 commit

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+
+FROM python
+MAINTAINER Smitha Madhav
+COPY Hi.py /opt/Hi.py
+CMD["python", "/opt/Hi.py]
+
---
+++ Hi.py
@@ -0,0 +1,9 @@
+FROM python
+MAINTAINER Smitha Madhav
+
+Import Time
+While  3> 2:
+	Print("Hello EY")
+	Time.sleep(5)
+	Print("hello Docker")
+
